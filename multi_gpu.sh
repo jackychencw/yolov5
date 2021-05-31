52ecfe681c8a87f88fcf01310651ff0664fc0e41
Ziqi Chen <jackychencw@gmail.com>
2021-05-31 14:46:04 -0400

    update

--- multi_gpu.sh
+++ multi_gpu.sh
@@ -1,3 +1,3 @@
 for i in 0 1 2 3; do
-  nohup python train.py --img 1920 --batch 16 --epochs 100 --data $SCRATCH/yolov5/data/spaghetti.yaml --cfg $SCRATCH/yolov5/models/yolov5l.yaml --weights "" --cache --evolve --device $i > evolve_gpu_$i.log &
+  nohup python train.py --img 1920 --batch 8 --epochs 100 --data ./data/spaghetti.yaml --hyp ./data/hyp.scratch.yaml --cfg ./models/yolov5l.yaml --weights "" --cache --evolve --device $i > evolve_gpu_$i.log &
 done
