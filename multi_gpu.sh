0b5bac50722e889eb5afffb8b971fd5b149f0d6c
Ziqi Chen <jackychencw@gmail.com>
2021-06-08 10:48:21 -0400

    update multi gpu script

--- multi_gpu.sh
+++ multi_gpu.sh
@@ -1,3 +1,3 @@
 for i in 0 1 2 3; do
-  nohup python train.py --img 1920 --batch 8 --epochs 100 --data ./data/spaghetti.yaml --hyp ./data/hyp.scratch.yaml --cfg ./models/yolov5l.yaml --weights "" --cache --evolve --device $i > evolve_gpu_$i.log &
+  nohup python train.py --img 1920 --batch 8 --epochs 1000 --data ./data/spaghetti.yaml --hyp ./data/hyp.scratch.yaml --cfg ./models/yolov5l.yaml --weights "" --cache --evolve --device $i > evolve_gpu_$i.log &
 done
--- script_hyp_tuning_multi.sh
+++ script_hyp_tuning_multi.sh
@@ -9,5 +9,7 @@ module load cuda/10.2.89 cudnn/7.6.5.32 anaconda3/2019.10 gcc/7.5.0
 source activate pytorch-gpu
 cd $SCRATCH/yolov5
 wandb offline
-bash multi_gpu.sh
+for i in 0 1 2 3; do
+  nohup python train.py --img 1920 --batch 8 --epochs 1000 --data ./data/spaghetti.yaml --hyp ./data/hyp.scratch.yaml --cfg ./models/yolov5l.yaml --weights "" --cache --evolve --device $i > evolve_gpu_$i.log &
+done
 conda deactivate
