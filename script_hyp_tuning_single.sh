5df0e9bfe4629173e6a8215261643cd2d1ca86cb
Ziqi Chen <jackychencw@gmail.com>
2021-06-08 10:50:31 -0400

    update single script

--- script_hyp_tuning_single.sh
+++ script_hyp_tuning_single.sh
@@ -2,11 +2,11 @@
 #SBATCH --nodes=1 # Cores proportional to GPUs: 6 on Cedar, 10 on Béluga, 16 on Graham.
 #SBATCH --gpus-per-node=1
 #SBATCH --output=./outputs/%N-%J.OUT
-#SBATCH --time=0-06:00     # DD-HH:MM:SS
+#SBATCH --time=0-24:00     # DD-HH:MM:SS
 
 module load cuda/10.2.89 cudnn/7.6.5.32 anaconda3/2019.10 gcc/7.5.0
 source activate pytorch-gpu
 cd $SCRATCH/yolov5
 wandb offline
-python train.py --img 1920 --batch 8 --epochs 1000 --data ./data/spaghetti.yaml --hyp ./data/hyp.scratch.yaml --cfg ./models/yolov5l.yaml --weights "" --cache --evolve
+python train.py --img 1920 --batch 8 --epochs 100 --data ./data/spaghetti.yaml --hyp ./data/hyp.scratch.yaml --cfg ./models/yolov5l.yaml --weights "" --cache --evolve
 conda deactivate
